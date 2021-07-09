278e1cade54727b87e84ed242fe123a233b65c08
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-07-09 17:06:39 +0300

    fix: in app.js remove winston from require

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const expressWinston = require('express-winston');
 const cookieParser = require('cookie-parser');
 const { isCelebrateError } = require('celebrate');
 const usersRouter = require('./routes/users');

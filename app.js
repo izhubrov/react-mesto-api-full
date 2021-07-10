7d7325cf63882305d31aa304f88aa9f636b960ba
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-07-10 08:01:07 +0300

    fix: port 3000

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const celebrateValidation = require('./helpers/celebrateValidation');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect(mongoUrl, mongoSettings);

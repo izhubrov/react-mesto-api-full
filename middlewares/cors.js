258357e38484e41928717aa2954741b83bbd1304
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-07-10 11:31:40 +0300

    fix: cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -21,7 +21,6 @@ const cors = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.status(200);
   }
 
   next();

a04866f6857bad759dd1ed6fda0b5f1b4b1ab8ce
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-07-10 08:27:18 +0300

    fix: domain

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,5 +1,5 @@
 const allowedCors = [
-  'https://izhubrov-mesto.nomoreparties.club',
+  'https://izhubrov-mesto.nomoredomains.club',
   'https://izhubrov.github.io',
   'http://localhost:3000',
 ];
@@ -7,21 +7,17 @@ const allowedCors = [
 const cors = (req, res, next) => {
   const { method, headers } = req;
   // ВЫНЕСТИ ПЕРЕМЕННУЮ В ENV!!!
-  // const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,POST,PUT,PATCH,DELETE';
-  // const requestHeaders = headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,POST,PUT,PATCH,DELETE';
+  const requestHeaders = headers['access-control-request-headers'];
   const { origin } = headers;
 
-  // if (allowedCors.includes(origin) && method === 'OPTIONS') {
-  //   res.set({
-  //     'Access-Control-Allow-Origin': origin,
-  //     'Access-Control-Allow-Methods': DEFAULT_ALLOWED_METHODS,
-  //     'Access-Control-Allow-Headers': requestHeaders,
-  //   });
-  // } else if (method !== 'OPTIONS' && allowedCors.includes(origin)) {
-  //   res.header('Access-Control-Allow-Origin', origin);
-  // }
-
-  if (method !== 'OPTIONS' && allowedCors.includes(origin)) {
+  if (allowedCors.includes(origin) && method === 'OPTIONS') {
+    res.set({
+      'Access-Control-Allow-Origin': origin,
+      'Access-Control-Allow-Methods': DEFAULT_ALLOWED_METHODS,
+      'Access-Control-Allow-Headers': requestHeaders,
+    });
+  } else if (method !== 'OPTIONS' && allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
 

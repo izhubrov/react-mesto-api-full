709175ed9bd3464ddf8577f6269f7b70f415db86
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-07-10 08:18:43 +0300

    fix: port 3000

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -7,17 +7,21 @@ const allowedCors = [
 const cors = (req, res, next) => {
   const { method, headers } = req;
   // ВЫНЕСТИ ПЕРЕМЕННУЮ В ENV!!!
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,POST,PUT,PATCH,DELETE';
-  const requestHeaders = headers['access-control-request-headers'];
+  // const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,POST,PUT,PATCH,DELETE';
+  // const requestHeaders = headers['access-control-request-headers'];
   const { origin } = headers;
 
-  if (allowedCors.includes(origin) && method === 'OPTIONS') {
-    res.set({
-      'Access-Control-Allow-Origin': origin,
-      'Access-Control-Allow-Methods': DEFAULT_ALLOWED_METHODS,
-      'Access-Control-Allow-Headers': requestHeaders,
-    });
-  } else if (method !== 'OPTIONS' && allowedCors.includes(origin)) {
+  // if (allowedCors.includes(origin) && method === 'OPTIONS') {
+  //   res.set({
+  //     'Access-Control-Allow-Origin': origin,
+  //     'Access-Control-Allow-Methods': DEFAULT_ALLOWED_METHODS,
+  //     'Access-Control-Allow-Headers': requestHeaders,
+  //   });
+  // } else if (method !== 'OPTIONS' && allowedCors.includes(origin)) {
+  //   res.header('Access-Control-Allow-Origin', origin);
+  // }
+
+  if (method !== 'OPTIONS' && allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
 

c913d13f68a41443758a22991351d14f75e6a964
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-07-10 10:37:19 +0300

    fix: cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,25 +1,26 @@
 const allowedCors = [
   'https://izhubrov-mesto.nomoredomains.club',
-  'http://izhubrov-mesto.nomoredomains.club',
+  'https://izhubrov.github.io',
+  'http://localhost:3000',
 ];
 
 const cors = (req, res, next) => {
-  const { origin } = req.headers;
+  const { method, headers } = req;
   // ВЫНЕСТИ ПЕРЕМЕННУЮ В ENV!!!
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,POST,PUT,PATCH,DELETE';
+  const requestHeaders = headers['access-control-request-headers'];
+  const { origin } = headers;
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.status(200);
   }
 
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,POST,PUT,PATCH,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-
   if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.status(200);
+    res.set({
+      // 'Access-Control-Allow-Origin': origin,
+      'Access-Control-Allow-Methods': DEFAULT_ALLOWED_METHODS,
+      'Access-Control-Allow-Headers': requestHeaders,
+    });
   }
 
   next();
